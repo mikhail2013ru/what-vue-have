6b0bcbc737ff477a928a1fba97e4cadabe2c6cc3
mikhail2013ru <mikhail2013ru@gmail.com>
2025-04-18 17:48:23 +0300

    2 блок пройден

--- app.js
+++ app.js
@@ -12,6 +12,29 @@ Vue.createApp({
             lastName: 'Volnov',
             age: 36
         },
-        items: [1,2,3,4,5,6]
-    })
+        items: [1, 2]
+    }),
+    methods: {
+        addItem(event) {
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+            console.log(event.key);            
+        }, 
+        remove(i) {
+            this.items.splice(i, 1)
+        },
+        log(item) {
+            console.log('Log item', item);            
+        }
+    },
+    computed: {
+        evenItems() {
+            return this.items.filter(i => i % 2 === 0)
+        }
+    }
+    // methods: {
+    //     stopPropagation(event) {
+    //         event.stopPropagation()
+    //     }
+    // }
 }).mount('#app')
